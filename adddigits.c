75413a54e4e47deb1075b24bbd221ca602ebc909
PriyankSukhadiya123 <priyankpatel5529@gmial.com>
2026-01-17 19:32:59 +0530

    commit

--- adddigits.c
+++ adddigits.c
@@ -3,13 +3,21 @@
 int addDigits(int num) {
     int reminder,sum=0;
 
-    while (num!= 0)
+    while (num!= 0 || sum>9)
     {
+        if (num==0)
+        {
+            num=sum;
+            sum=0;
+        }
+        
         reminder = num%10;
         sum = sum + reminder;
         num /=10;
         
     }
+    
+    
 
     return sum;
 
