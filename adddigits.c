2cafebe85f5173b93586e898ac26e49e5d989010
PriyankSukhadiya123 <priyankpatel5529@gmial.com>
2026-01-17 19:23:00 +0530

    commit

---
+++ adddigits.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+
+int addDigits(int num) {
+    int reminder,sum=0;
+
+    while (num!= 0)
+    {
+        reminder = num%10;
+        sum = sum + reminder;
+        num /=10;
+        
+    }
+
+    return sum;
+
+}
+
+int main(){
+    int num;
+    printf("enter number: ");
+    scanf("%d",&num);
+    int sum = addDigits(num);
+
+    printf("%d",sum);
+}
